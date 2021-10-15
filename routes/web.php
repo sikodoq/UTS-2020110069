f7d3940bd9ff3e9d0b8c499ff05c238030961738
Andi Wijaya <andi_wijaya@unpas.ac.id>
2021-10-15 23:47:26 +0700

    Menambah Route Home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
+
+Route::redirect('/', '/home', 301);
+
+Route::get('/home', 'HomeController@index');
+Route::get('/home/search', 'HomeController@search');
