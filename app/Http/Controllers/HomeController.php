4a1a602bdbe46c577b3ccc438356a7c779b1c358
Andi Wijaya <andi_wijaya@unpas.ac.id>
2021-10-16 03:06:22 +0700

    Membuat Function Index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $result = DB::table('pokemons')->orderBy('id', 'asc')->get();
+        return view('Home', ['posts' => $result]);
+    }
 }
